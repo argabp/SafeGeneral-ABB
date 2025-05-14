291e7e47d2e27317fae3365c56d60da4dc9c6d6d
arga <bagusarga5@gmail.com>
2025-05-15 01:09:19 +0700

    Fix Dashboard

--- ABB.Web/Modules/DashboardUnderwriting/JS/dashboardUnderwriting.js
+++ ABB.Web/Modules/DashboardUnderwriting/JS/dashboardUnderwriting.js
@@ -1,15 +1,41 @@
+function formatValue(value){
+    return value < 0
+        ? `(${Math.abs(value).toFixed(2)})`  // If negative: (0.82)
+        : value.toFixed(2);
+}
+
 $(document).ready(function () {
     var dashboardData = JSON.parse($("#dashboardData").val());
     var produksi = [];
     var target = [];
     var nm_cob = [];
     
+    var target_rkap = 0;
+    var produksi_sdthnini = 0;
+    var produksi_sdthnlalu = 0;
+    var produksi_blnini = 0;
+    var produksi_blnlalu = 0;
+    
     dashboardData.forEach(data => {
         produksi.push((data.produksi_sdthnini / 1000000));
         target.push((data.target_rkap / 1000000));
         nm_cob.push(data.nm_cob.replace(" ", "<br>"));
+        target_rkap += data.target_rkap;
+        produksi_sdthnini += data.produksi_sdthnini;
+        produksi_sdthnlalu += data.produksi_sdthnlalu;
+        produksi_blnini += data.produksi_blnini;
+        produksi_blnlalu += data.produksi_blnlalu;
     });
     
+    $("#target_rkap").text(formatValue(target_rkap));
+    $(".produksi_sdthnini").text(formatValue(produksi_sdthnini));
+    $("#rasio").text(formatValue(produksi_sdthnini / target_rkap) + "%");
+    $("#produksi_sdthnlalu").text(formatValue(produksi_sdthnlalu));
+    $("#produksi_naikTurunPerTahun").text(formatValue((produksi_sdthnini - produksi_sdthnlalu) / produksi_sdthnlalu) + "%");
+    $("#produksi_blnini").text(formatValue(produksi_blnini));
+    $("#produksi_blnlalu").text(formatValue(produksi_blnlalu));
+    $("#produksi_naikTurunPerBulan").text(formatValue((produksi_blnini - produksi_blnlalu) / produksi_blnlalu) + "%");
+
     // ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"];
 
     let chartConfig = {
